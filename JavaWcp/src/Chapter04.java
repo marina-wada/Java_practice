1ebca2a1969147b4ea842e19a1432cfe148e5d3d
marina <wdmrna4i1@gmail.com>
2022-03-09 23:43:59 +0900

    [Finish]basic_03 basic_04

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+
+	public static void main(String[]args) {
+		String str = "Wada";
+		final int lucky_number = 8;
+		System.out.println(str);
+		str = "Happy";
+		System.out.println(str);
+		System.out.println(lucky_number);
+	}
+
+}
