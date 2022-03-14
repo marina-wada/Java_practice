d205af7eaf4fb019456c3e2f899d0e4dd6e87574
marina <wdmrna4i1@gmail.com>
2022-03-14 18:10:42 +0900

    [Finish]Java_basic_05

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 1234567891234567L;
+		double double1 = 123.56d;
+		boolean boolean1 = true;
+		Integer integer1 = 5;
+		Float float1 = 0.25f;
+		Character character1 = 'あ';
+		int[] array = { 0,10,20,30,40};
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		System.out.println(integer1);
+		System.out.println(float1);
+		System.out.println(character1);
+		System.out.println(array[3]);
+
+	}
+
+}
