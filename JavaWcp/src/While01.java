1cf79a43f8601a6f70ba142ef9490eaacd60990f
marina <wdmrna4i1@gmail.com>
2022-04-05 14:57:01 +0900

    [Start]Basic_Java_08

---
+++ JavaWcp/src/While01.java
@@ -0,0 +1,11 @@
+
+public class While01 {
+	public static void main(String[] args) {
+		int number = 50;
+		while (number < 50) {
+			number *= 3;
+			System.out.println("While01 = " + number);
+		}
+	}
+
+}
