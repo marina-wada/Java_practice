72d8e0059b777f43a649957e606a9298943238e1
marina <wdmrna4i1@gmail.com>
2022-05-08 23:31:48 +0900

    [Finish]Sukkiri_Java3

--- JavaWcp/src/Sukkiri_main3.java
+++ JavaWcp/src/Sukkiri_main3.java
@@ -107,14 +107,54 @@ public class Sukkiri_main3 {
 		*/
 
 		//p3-4
+		/*
 		boolean tenki = true;
 		if (tenki == true) {
 			System.out.println("洗濯をします");
 			System.out.println("散歩に行きます");
-		} else
+		} else {
 			System.out.println("DVDを見ます");
 			System.out.println("寝ます");
+		}
+		*/
 
+		//3-5
+		/*
+		System.out.println("[メニュー] 1:検索 2:登録 3:削除 4:変更>");
+		int selected = new java.util.Scanner(System.in).nextInt();
+		switch (selected) {
+		case 1:
+			System.out.print("検索します");
+			break;
+
+		case 2:
+			System.out.print("登録します");
+			break;
+
+		case 3:
+			System.out.print("削除します");
+			break;
+
+		case 4:
+			System.out.print("変更します");
+			break;
+		}
+		*/
+
+		//3-6
+		System.out.println("【数あてゲーム】");
+		int ans = new java.util.Random().nextInt(10);
+		for (int i = 0; i < 5; i++) {
+			System.out.println("0〜9の数字を入力してください");
+			int num = new java.util.Scanner(System.in).nextInt();
+			if (num == ans) {
+				System.out.println("アタリ！");
+				break;
+			} else {
+				System.out.println("違います");
+			}
+		}
+		System .out.println("ゲームを終了します");
 	}
 
 
