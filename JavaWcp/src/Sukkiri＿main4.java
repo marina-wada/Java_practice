f57d3c72749051d737c1580b9c9118d29e904bf8
marina <wdmrna4i1@gmail.com>
2022-05-10 14:01:53 +0900

    [Start]Sukkiri_java4

---
+++ JavaWcp/src/Sukkiri＿main4.java
@@ -0,0 +1,24 @@
+
+public class Sukkiri＿main4 {
+	public static void main(String[] args) {
+		//4-4
+		/*
+		int[] scores = new int[5];
+		int num = scores.length;
+		System.out.println("要素の数: " + num);
+		*/
+
+		//4-5
+		/*
+		int[] scores;
+		scores = new int[5];
+		scores[1] = 30;
+		System.out.println(scores[1]);
+		*/
+
+		//4-7
+		int[] scores = new int[5];
+		System.out.println(scores[0]);
+	}
+
+}
